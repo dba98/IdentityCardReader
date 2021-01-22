6124ca27e5cd19ddbd4e4544c40baeee9c2f0aaf
Josué Braz <38355244+JsBraz@users.noreply.github.com>
2021-01-22 17:45:17 +0000

    profile page fix

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -13,6 +13,8 @@ import {useStyles} from './Styles';
 import {withStyles} from '@material-ui/core/styles';
 import {borders} from '@material-ui/system';
 
+
+
 export const isAuth = () => {
         return localStorage.getItem('username');
 }
@@ -35,9 +37,16 @@ const Profile = props => {
                 <Box border={1} borderRadius={"borderRadius"} borderColor="pink" boxShadow={3}
                      style={{padding: '75px'}}>
                     <h1>Your profile</h1>
-                    <text>{userName} </text>
-                    <text>{token}</text>
-
+                    <table>
+                    <tr>
+                        <td>userName :</td>
+                        <td >{userName}</td>
+                    </tr>
+                    <tr>
+                        <td>token :</td>
+                        <td>{token}</td>  
+                    </tr>
+                    </table>
                 </Box>
             </Grid>
         </Container>
